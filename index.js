1a49e10029c3269e955ab7f3a01d2e994626b866
timocodex <timothy.og.siahaan@gmail.com>
2017-01-21 12:39:28 +0700

    tambahan test code

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ const db = require('./models')
 //   })
 // })
 
-db.Teacher.findById(3).then(function(teach){
-  db.Student.findById(2).then(function(student){
-    student.addTeachers(teach)
+// db.Teacher.findById(1).then(function(teach){
+//   db.Student.findById(4).then(function(student){
+//     student.addTeachers(teach)
+//   })
+// })
+
+
+function findMyStudents(teacherId){
+  db.Teacher.findById(teacherId).then(function(stutea){
+      stutea.getStudents().then(function(stu){
+        stu.forEach(function(s){
+          console.log(`student id: ${s.id}, name: ${s.name}`)
+        })
+      })
   })
-})
+}
+
+function findMyTeachers(studentId){
+  db.Student.findById(studentId).then(function(stutea){
+      stutea.getTeachers().then(function(tea){
+        tea.forEach(function(t){
+          console.log(`teacher id: ${t.id}, name: ${t.name}`)
+        })
+      })
+  })
+}
+
+findMyStudents(1)
+findMyTeachers(2)
