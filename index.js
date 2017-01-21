560617b0e92f0a3fe64e86c466363c4128ee75df
Syanmil <syanmil@yahoo.com>
2017-01-21 10:22:01 +0700

    manualy create association each student for teacher

--- index.js
+++ index.js
@@ -9,16 +9,94 @@ const faker = require('faker')
 //     console.log(teacher.dataValues);
 //   });
 // })
-
-// db.Student.bulkCreate([
-//   {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), TeacherId: Math.floor(Math.random()*20+1), phone: '1234567891012', height: 150},
-//   {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), TeacherId: Math.floor(Math.random()*20+1), phone: '1234567891012', height: 150},
-//   {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), TeacherId: Math.floor(Math.random()*20+1), phone: '1234567891012', height: 150},
-//   {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), TeacherId: Math.floor(Math.random()*20+1), phone: '1234567891012', height: 150}
-// ])
+// function createDummy(number) {
+//   for (let i = 0; i < number; i++){
+//     db.Student.bulkCreate([
+//       {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), phone: '1234567891012', height: 150},
+//       {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), phone: '1234567891012', height: 150},
+//       {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), phone: '1234567891012', height: 150},
+//       {firstName: faker.name.firstName(), lastName: faker.name.lastName(), birthday: faker.date.between('1990-01-01', '2000-01-05'), phone: '1234567891012', height: 150},
+//     ])
+//   }
+// }
+// createDummy(5)
 //
 // db.Teacher.bulkCreate( [
 //     {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}, {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}, {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()},
 //     {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}, {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}, {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()},
 //     {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}, {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}, {name: faker.name.findName(), phone: '1234567891012', email:faker.internet.email()}
 //   ])
+function matching() {
+  for (let j = 1; j < 21; j++){
+    db.Student_Teacher.create({
+      teacherId: Math.ceil(Math.random()*9),
+      studentId: j
+    })
+  }
+}
+// matching()
+
+function teacherOf(studentId) {
+  db.Student.findOne({
+    where: {
+      id: studentId
+    },
+    attributes: ['id', 'firstName']
+  }).then((student) => {
+    console.log(`Teacher(s) Of ${student.dataValues.firstName} :`);
+    db.student_teacher.findAll({
+      where: {
+        StudentId: student.dataValues.id
+      },
+      attributes: ['teacherId']
+    }).then((teacher) => {
+      for (var i = 0; i < teacher.length; i++) {
+        db.Teacher.findAll({
+          where: {
+            id: teacher[i].dataValues.teacherId
+          },
+          attributes: ['name']
+        })
+        .then((data) => {
+          for (var i = 0; i < data.length; i++) {
+            console.log(data[i].dataValues.name);
+          }
+        })
+      }
+    })
+  })
+}
+
+function studentOf(teacherId) {
+  db.Teacher.findOne({
+    where: {
+      id: teacherId
+    },
+    attributes: ['id', 'name']
+  }).then((teacher) => {
+    console.log(`Student(s) Of ${teacher.dataValues.name} :`);
+    db.student_teacher.findAll({
+      where: {
+        teacherId: teacher.dataValues.id
+      },
+      attributes: ['studentId']
+    }).then((student) => {
+      for (var i = 0; i < student.length; i++) {
+        db.Student.findAll({
+          where: {
+            id: student[i].dataValues.studentId
+          },
+          attributes: ['firstName']
+        })
+        .then((data) => {
+          for (var i = 0; i < data.length; i++) {
+            console.log(data[i].dataValues.firstName);
+          }
+        })
+      }
+    })
+  })
+}
+
+teacherOf(8)
+studentOf(3)
